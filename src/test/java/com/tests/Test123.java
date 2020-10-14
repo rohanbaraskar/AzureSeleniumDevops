5c69b66935f58a5efb2d5194aa5da9633a37b9d9
Sathish Raghuram <sathish.raghuram@zenq.com>
2020-10-14 13:29:31 +0530

    first commit for azure devops

--- src/test/java/com/tests/Test123.java
+++ src/test/java/com/tests/Test123.java
@@ -1,4 +1,11 @@
 package com.tests;
 
 public class Test123 {
+
+    public static void main(String[] args) {
+        int[] numbers = {45,23,78,12,56,90};
+        for(int i=0;i<numbers.length;i++) {
+            System.out.println(numbers[i]);
+        }
+    }
 }
